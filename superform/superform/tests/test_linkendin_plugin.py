d1c56e14979115e76115d2c14c91a4a4b6f1c5f8
romanskz <skubiszewski.roman@gmail.com>
2018-10-25 21:22:17 +0200

    Correction test plugin linkedin

--- superform/superform/tests/test_linkendin_plugin.py
+++ superform/superform/tests/test_linkendin_plugin.py
@@ -40,7 +40,7 @@ def test_authenticate2(client):
     linkedin_access_token = "test_token"
     linkedin_token_expiration_date = datetime.now() +timedelta(seconds=60)
     channel_name = "channel_test"
-    c_test = Channel(id=1, name=channel_name, module=get_module_full_name("linkedin"), config="{}", linkedin_access_token=linkedin_access_token, linkedin_token_expiration_date=linkedin_token_expiration_date)
+    c_test = Channel(name=channel_name, module=get_module_full_name("linkedin"), config="{}", linkedin_access_token=linkedin_access_token, linkedin_token_expiration_date=linkedin_token_expiration_date)
     db.session.add(c_test)
     linkedin.LinkedinTokens.put_token(linkedin.LinkedinTokens, "channel_test", linkedin_access_token, linkedin_token_expiration_date)
     false_post_id = -1
