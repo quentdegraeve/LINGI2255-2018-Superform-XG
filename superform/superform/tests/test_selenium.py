1687e7f62697ce0301a1d58bae46bcd889283ccf
feljac <felixjacoby@yahoo.fr>
2018-11-09 14:12:38 +0100

    test_selenium updated

--- superform/superform/tests/test_selenium.py
+++ superform/superform/tests/test_selenium.py
@@ -1,6 +1,7 @@
 from superform.suputils import selenium_utils
 from superform.models import Channel
 from superform.suputils import keepass
+from time import sleep
 
 
 #Create a channel and add 2 post (moderation and publication have to be done manually)
@@ -9,7 +10,14 @@ def test_complete_linkedin():
     selenium_utils.login(driver, 'superego', 'superego')
     keepass.set_entry_from_keepass('account_linkedin')
     selenium_utils.create_channel(driver, 'test_linkedin', keepass.KeepassEntry.username, keepass.KeepassEntry.password, 'linkedin')
+    sleep(1)
+    selenium_utils.create_channel(driver, 'test_slack', 'raphael.hacha@student.uclouvain.be', keepass.KeepassEntry.password, 'slack')
+    sleep(1)
+    selenium_utils.create_channel(driver, 'test_chan_slack', 'raphael.hacha@student.uclouvain.be', keepass.KeepassEntry.password, 'slack')
+    sleep(1)
     selenium_utils.add_authorization(driver, 'test_linkedin', 'superego', 2)
-    selenium_utils.add_new_post(driver, 'test_linkedin', 'test title1', 'test description1', '', '', '08112018', '09112018')
-    selenium_utils.add_new_post(driver, 'test_linkedin', 'test title2', 'test description2', '', '', '08112018', '09112018')
+    sleep(1)
+    selenium_utils.add_authorization(driver, 'test_slack', 'superego', 2)
+    sleep(1)
+    selenium_utils.add_authorization(driver, 'test_chan_slack', 'superego', 2)
     assert 1 == 1
