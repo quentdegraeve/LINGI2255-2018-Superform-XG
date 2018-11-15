b283ffe5a014b8350e8ab2ce62af605881d386c3
Jimmy <jimmy.delacruz@student.uclouvain.be>
2018-11-15 13:40:46 +0100

    resolve bug test prevalidate

--- superform/superform/tests/test_plugin_utils.py
+++ superform/superform/tests/test_plugin_utils.py
@@ -7,6 +7,8 @@ def test_pre_validate_post_title():
     post = Post
     post.title = "x" * 200
     post.description = "x"
+    post.link_url = "https://www.test.com"
+    post.image_url ="https://www.test.com"
     assert plugin_utils.post_pre_validation_plugins(post,200,256) == 1
     post.title += "x"
     assert  plugin_utils.post_pre_validation_plugins(post,200,256) == 0
@@ -18,6 +20,8 @@ def test_pre_validate_post_description():
     post = Post
     post.title = "x"
     post.description = "x" * 256
+    post.link_url = "https://www.test.com"
+    post.image_url = "https://www.test.com"
     assert  plugin_utils.post_pre_validation_plugins(post,200,256) == 1
     post.description += "x"
     assert  plugin_utils.post_pre_validation_plugins(post,200,256) == 0
@@ -29,16 +33,22 @@ def test_pre_validate_post_Link_url():
     post.title = "x"
     post.description = "x"
     post.link_url = "https://www.test.com"
+    post.image_url = "https://www.test.com"
     assert  plugin_utils.post_pre_validation_plugins(post,200,256) == 1
-    post.link_url += "test error link"
+    post.link_url = "test error link"
     assert  plugin_utils.post_pre_validation_plugins(post,200,256) == 0
+    post.link_url = ""
+    assert plugin_utils.post_pre_validation_plugins(post, 200, 256) == 1
 
-def test_pre_validate_post_Link_url():
+def test_pre_validate_post_img_url():
     post = Post
     post.title = "x"
     post.description = "x"
     post.link_url = "https://www.test.com"
     post.image_url = "https://www.test.com"
     assert  plugin_utils.post_pre_validation_plugins(post,200,256) == 1
-    post.image_url += "test error link"
+    post.image_url = "test error link"
     assert  plugin_utils.post_pre_validation_plugins(post,200,256) == 0
+    post.link_url = ""
+    assert plugin_utils.post_pre_validation_plugins(post, 200, 256) == 1
+
