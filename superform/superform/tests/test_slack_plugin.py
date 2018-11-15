7a299b62254839405a8adcaf943d3edd9db9aeb6
Jimmy <jimmy.delacruz@student.uclouvain.be>
2018-11-15 15:11:59 +0100

    add test slack

--- superform/superform/tests/test_slack_plugin.py
+++ superform/superform/tests/test_slack_plugin.py
@@ -4,20 +4,21 @@ from superform.suputils import plugin_utils
 
 def test_pre_validate_post_title():
     chan = Channel
-    chan.module = "superform.plugins.linkedin"
-    plugin_utils.test_pre_validate_post_title(chan,200)
+    chan.module = "superform.plugins.slack"
+    plugin_utils.test_pre_validate_post_title(chan,40000)
+
 
 def test_pre_validate_post_description():
     chan = Channel
-    chan.module = "superform.plugins.linkedin"
-    plugin_utils.test_pre_validate_post_description(chan,256)
+    chan.module = "superform.plugins.slack"
+    plugin_utils.test_pre_validate_post_description(chan,40000)
 
 def test_prevalidate_post_link_url():
     chan = Channel
-    chan.module = "superform.plugins.linkedin"
+    chan.module = "superform.plugins.slack"
     plugin_utils.test_pre_validate_post_Link_url(chan)
 
 def test_prevalidate_post_img_url():
     chan = Channel
-    chan.module = "superform.plugins.linkedin"
+    chan.module = "superform.plugins.slack"
     plugin_utils.test_pre_validate_post_img_url(chan)
