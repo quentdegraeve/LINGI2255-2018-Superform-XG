78b74acdfa4b14aa25f2a150a66f5995991c5dd1
romanskz <skubiszewski.roman@gmail.com>
2018-11-26 23:23:38 +0100

    Correction models

--- superform/superform/models.py
+++ superform/superform/models.py
@@ -2,6 +2,8 @@ from flask_sqlalchemy import SQLAlchemy
 from enum import Enum
 import datetime
 
+from sqlalchemy import UniqueConstraint
+
 db = SQLAlchemy()
 
 
@@ -50,8 +52,8 @@ class Post(db.Model):
 
 
 class Publishing(db.Model):
-    publishing_id = db.Column(db.Integer, primary_key=True, nullable=False)
-    num_version = db.Column(db.Integer, unique=True, nullable=False)
+    publishing_id = db.Column(db.Integer, autoincrement=True, primary_key=True, unique=True, nullable=False)
+    num_version = db.Column(db.Integer, nullable=False)
     post_id = db.Column(db.Integer, db.ForeignKey("post.id"), nullable=False)
     channel_id = db.Column(db.Integer, db.ForeignKey("channel.id"), nullable=False)
     state = db.Column(db.Integer, nullable=False, default=-1)
@@ -61,6 +63,9 @@ class Publishing(db.Model):
     image_url = db.Column(db.Text)
     date_from = db.Column(db.DateTime)
     date_until = db.Column(db.DateTime)
+
+    UniqueConstraint(num_version, post_id, channel_id, name='unicity_publishing_numvers')
+    __table_args__ = ({"sqlite_autoincrement": True},)
 
     def __repr__(self):
         return '<Publishing {} {}>'.format(repr(self.post_id), repr(self.channel_id))
@@ -118,6 +123,7 @@ class Comment(db.Model):
         return '<Comment {}>'.format(repr(self.publishing_id))
 
     __table_args__ = (db.PrimaryKeyConstraint('publishing_id'),)
+
 
 class Permission(Enum):
     AUTHOR = 1
