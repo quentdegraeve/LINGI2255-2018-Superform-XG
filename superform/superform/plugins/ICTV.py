40be42e72b0b47ea5831b7aa701311271577ac05
ffran <francois.froment@student.uclouvain.be>
2018-12-07 16:58:42 +0100

    ICTV: fixed the edit of ictv

--- superform/superform/plugins/ICTV.py
+++ superform/superform/plugins/ICTV.py
@@ -99,14 +99,16 @@ def edit(publishing, channel_config):
     date_from=str(date_from)
     val_until=int(time.mktime(time.strptime(date_until,'%Y-%m-%d %H:%M:%S'))) - time.timezone
     val_from = int(time.mktime(time.strptime(date_from, '%Y-%m-%d %H:%M:%S'))) - time.timezone
-    partial_capsules = json.dumps({"name": title, "theme": "ictv", "validity": [val_from,val_until]})
+    partial_capsules = json.dumps({"name": capsule_title+title, "theme": "ictv", "validity": [val_from,val_until]})
     response = requests.patch(url + "/" + str(capsule_id), data=partial_capsules, headers=header_patch)
     if response.status_code != 204:
         print("HttpError_edit1: " + str(response.status_code))
         return
     template = template_selector(image_url)
     slide = create_a_slide(duration, template, title, subtitle, text, logo, image_url)
-    response = requests.patch(url + "/" + str(capsule_id) + "/slides/1", data=json.dumps(slide), headers=header_patch)
+    slide_id = get_slide_id(url, header_get, capsule_id)
+    print("slide_id :" + str(slide_id))
+    response = requests.patch(url + "/" + str(capsule_id) + "/slides/" + str(slide_id), data=json.dumps(slide), headers=header_patch)
     if response.status_code != 204:
         print("HttpError_edit2: " + str(response.status_code))
         return
@@ -121,6 +123,19 @@ def get_capsule_id(url, header, capsule_title):
     for elem in capsules:
         if capsule_title in elem.get("name"):
             return str(elem.get("id"))
+    return -1
+
+
+def get_slide_id(url, header, capsule_id):
+    all_capsules = requests.get(url, headers=header)
+    if all_capsules.status_code != 200:
+        print("HttpError_get_capsule_id: " + all_capsules.status_code)
+        return -1
+    capsules = all_capsules.json()
+    for elem in capsules:
+        if str(capsule_id) == str(elem.get("id")):
+            return str(elem.get("slides")[0].get("id"))
+    return -1
 
 
 def run(publishing, channel_config):
@@ -131,7 +146,6 @@ def run(publishing, channel_config):
         if json_data.get(field) is None:
             print("Missing : {0}".format(field))
             return
-
     duration = int(publishing.duration)
     title = publishing.title
     subtitle = publishing.subtitle
@@ -144,6 +158,12 @@ def run(publishing, channel_config):
     header_get = {'accept': 'application/json', 'X-ICTV-editor-API-Key': json_data.get('api_key')}
     header_post = {'accept': 'application/json','Content-Type': 'application/json', 'X-ICTV-editor-API-Key': json_data.get('api_key')}
     capsule_title = "Superform_post" + str(publishing.post_id) + ":" + title
+
+    if get_capsule_id(url, header_get, "Superform_post" + str(publishing.post_id) + ":") != -1:
+        edit(publishing, channel_config)
+        publishing.state = 1
+        db.session.commit()
+        return
 
     try:
         full_capsule = creates_a_capsule(url,header_post,date_from,date_until,capsule_title)
